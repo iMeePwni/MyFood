632ddea3eb76441382e02b8d2d4f0ffe32343da1
guofeng <guofeng@gcall.org>
2018-04-18 09:16:16 +0800

    1）增加获取全部菜单标签测试用例。

--- app/src/test/java/com/imeepwni/myfood/ExampleUnitTest.kt
+++ app/src/test/java/com/imeepwni/myfood/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.imeepwni.myfood
 
+import com.google.gson.Gson
+import com.imeepwni.myfood.model.data.GetMenuTabsBean
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.io.File
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    /**
+     * 检测获取全部菜单标签Bean类转化
+     */
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testGetMenuTabsBean() {
+        val stringBuilder = StringBuilder()
+        File("E:\\MyFood\\app\\src\\test\\java\\com\\imeepwni\\myfood\\data\\getMenuTabsBean").forEachLine {
+            stringBuilder.append(it)
+        }
+        var getMenuTabsBean: GetMenuTabsBean? = null
+        try {
+            getMenuTabsBean = Gson().fromJson(stringBuilder.toString(), GetMenuTabsBean::class.java)
+            print(getMenuTabsBean)
+        } catch (e: Exception) {
+            print(e)
+        }
+        assert(getMenuTabsBean != null)
     }
 }
